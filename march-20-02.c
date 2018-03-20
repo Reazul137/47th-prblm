4a8e9c616c5a7bab41f667a985c3374cb911aaff
Reazul Islam <reazulislam137@gmail.com>
2018-03-20 21:31:52 +0600

    first commit

---
+++ march-20-02.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    int x, i;
+
+    printf("Input an integer : ");
+    scanf("%d",&x);
+
+    printf("All the divisor of %d are: ", x);
+	for(i = 1; i <= x; i++) {
+		if((x%i) == 0){
+			printf("\n%d", i);
+			printf("\n");
+		}
+	}
+
+    return 0;
+}
